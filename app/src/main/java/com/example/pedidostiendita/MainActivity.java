85412df2e168287daf2251f7d6819384052b04b2
Jaime <correostudent12@gmail.com>
2022-03-26 16:59:59 -0600

    diseño

--- app/src/main/java/com/example/pedidostiendita/MainActivity.java
+++ app/src/main/java/com/example/pedidostiendita/MainActivity.java
@@ -100,7 +100,7 @@ public class MainActivity extends AppCompatActivity {
                     RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
                     requestQueue.add(stringRequest);
 
-                
+
 
 
             }
--- app/src/main/java/com/example/pedidostiendita/splash.java
+++ app/src/main/java/com/example/pedidostiendita/splash.java
@@ -2,8 +2,14 @@ package com.example.pedidostiendita;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class splash extends AppCompatActivity {
 
@@ -15,6 +21,21 @@ public class splash extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
 
         spla = (ImageView) findViewById(R.id.IVs);
+
+        Glide.with(getApplicationContext()).load(R.drawable.splash).into(spla);
+
+        TimerTask splash = new TimerTask() {
+            @Override
+            public void run() {
+                Intent i = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        };
+        Timer segundos = new Timer();
+        segundos.schedule(splash,4000);
+
+
 
     }
 }
